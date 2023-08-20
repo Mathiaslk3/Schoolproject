5a34107673586f67e157512b907005a9669dde9b
Mathiaslk3 <mathiaslk3@outlook.dk>
2023-08-20 16:05:10 +0200

    Add(Custom URL for login page)
    TODO: Show content when on the custom URL

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestWebsite.Controllers
+{
+    public class LoginController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/Account/Login")]
+        public IActionResult Post()
+        {
+            return new ContentResult { 
+                
+            };
+        }
+    }
+}
